cedce6cd44ceed14884f4f18702dd146e855fa0c
Michal Tulej <michaltulej1@gmail.com>
2024-05-12 18:30:40 +0200

    Add project files.

---
+++ AuctionAce.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuctionAce.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
